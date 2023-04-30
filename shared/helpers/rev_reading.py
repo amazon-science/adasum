b5d000021392d34443f463e9eda4d85fab67b57b
abrazinskas <bulletdll@gmail.com>
2023-04-30 23:36:14 +0100

    Added a function to pre-process FewSum reviews and updated the readme file.

--- shared/helpers/rev_reading.py
+++ shared/helpers/rev_reading.py
@@ -1,4 +1,5 @@
 from shared.helpers.io import parse_gz
+from zipfile import ZipFile
 from shared.constants.general import SRC, TGT
 from shared.helpers.cleaning import clean_rev_text
 from shared.constants.amazon import REVIEW_TEXT as AMA_REVIEW_TEXT, \
@@ -9,9 +10,39 @@ from shared.constants.yelp import REVIEW_TEXT as YELP_REVIEW_TEXT, \
     BUSINESS_ID as YELP_BUSINESS_ID, REVIEW_ID as YELP_REVIEW_ID,\
     RATING as YELP_RATING
 from shared.helpers.general import is_in_range
+from shared.helpers.io import read_table_data
 from collections import defaultdict
 import os
 import json
+
+
+def iter_fewsum_revs(input_data_path, dataset='amazon'):
+    """"Iterates over reviews in a FewSum file."""
+    counter = 0
+
+    if dataset == "amazon":
+        entity_id_fname = AMA_ASIN
+        review_text = AMA_REVIEW_TEXT
+        reviewer_id = AMA_REVIEWER_ID
+    else:
+        entity_id_fname = YELP_BUSINESS_ID
+        review_text = YELP_REVIEW_TEXT
+        reviewer_id = YELP_REVIEW_ID
+
+    with ZipFile(input_data_path, 'r') as zip:
+        file_names = sorted(zip.namelist())
+        for file_name in file_names:
+            if file_name.split(".")[-1] != "csv":
+                continue
+            with zip.open(file_name) as f:
+                data = read_table_data(f)
+                for indx, entry in data.iterrows():
+                    new_entry = {}
+                    counter += 1
+                    new_entry[entity_id_fname] = entry['group_id'].split("_")[0]
+                    new_entry[review_text] = entry['review_text']
+                    new_entry[reviewer_id] = counter
+                    yield new_entry
 
 
 def iter_ama_revs(input_data_path, clean_text=False, verified_only=False):
@@ -76,24 +107,32 @@ def read_amazon_revs(input_file_paths, src_rev_min_len=None,
                      src_rev_max_len=None, tgt_rev_min_len=None,
                      tgt_rev_max_len=None, verified=False, limit=None,
                      tokenizer=None):
-    """Reads Amazon reviews from the .gz files.
+    """Reads Amazon reviews from the .gz files or .zip (FewSum).
 
     Returns:
         dict ASINs mapped to source and target reviews.
      """
+    if isinstance(input_file_paths, str):
+        input_file_paths = [input_file_paths]
     tok = lambda x: (tokenizer(x) if tokenizer is not None else x.split())
-    # asins mapped to source and target reviews
+    # ASINs mapped to source and target reviews
     coll = defaultdict(lambda: {SRC: [], TGT: []})
 
     dupl_tracker = defaultdict(lambda: {AMA_REVIEWER_ID: set(),
                                         AMA_REVIEW_TEXT: set()})
 
+    review_count = 0
     for input_file_path in input_file_paths:
+
         # reading and filtering
-        ama_iter = iter_ama_revs(input_file_path, verified_only=verified,
-                                 clean_text=True)
+        if input_file_path.split(".")[-1] == "zip":
+            ama_iter = iter_fewsum_revs(input_file_path, dataset='amazon')
+        else:
+            ama_iter = iter_ama_revs(input_file_path, verified_only=verified,
+                                     clean_text=True)
+
         for indx, rev in enumerate(ama_iter):
-            if limit is not None and len(coll) >= limit:
+            if limit is not None and review_count >= limit:
                 break
 
             asin = rev[AMA_ASIN]
@@ -119,6 +158,8 @@ def read_amazon_revs(input_file_paths, src_rev_min_len=None,
                            max=tgt_rev_max_len):
                 coll[asin][TGT].append((indx, rev))
 
+            review_count += 1
+
     return coll
 
 
@@ -130,9 +171,18 @@ def read_yelp_revs(input_file_path, src_rev_min_len=None,
     coll = defaultdict(lambda: {SRC: [], TGT: []})
 
     buss_to_text = defaultdict(lambda: set())
-    rev_iter = iter_yelp_revs(input_file_path, clean_text=True)
+
+    # reading and filtering
+    if input_file_path.split(".")[-1] == "zip":
+        rev_iter = iter_fewsum_revs(input_file_path, dataset='yelp')
+    else:
+        rev_iter = iter_yelp_revs(input_file_path, clean_text=True)
+
+    review_count = 0
+
     for (indx, rev) in enumerate(rev_iter):
-        if limit is not None and len(coll) >= limit:
+
+        if limit is not None and review_count >= limit:
             break
 
         bus_id = rev[YELP_BUSINESS_ID]
@@ -141,7 +191,7 @@ def read_yelp_revs(input_file_path, src_rev_min_len=None,
 
         # standartization
         rev[AMA_REVIEW_TEXT] = rev_text
-        rev[AMA_RATING] = rev[YELP_RATING]
+        # rev[AMA_RATING] = rev[YELP_RATING]
 
         # avoiding duplicates
         if rev_text in buss_to_text[bus_id]:
@@ -156,5 +206,6 @@ def read_yelp_revs(input_file_path, src_rev_min_len=None,
             coll[bus_id][TGT].append((indx, rev))
 
         buss_to_text[bus_id].add(rev_text)
+        review_count += 1
 
     return coll
